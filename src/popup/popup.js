2005c15283135e876c71d4b05985a045b1d63e2d
BuildTools <unconfigured@null.spigotmc.org>
2023-03-03 18:45:16 +0700

    first commit

---
+++ src/popup/popup.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// const link = document.getElementById('13xlabs-link');
+// const linkgotoChatGPT = document.getElementById('13xlabs-go-to-chatgpt');
+
+// async function getCurrentTab() {
+//   const queryOptions = { active: true, currentWindow: true };
+//   const [tab] = await chrome.tabs.query(queryOptions);
+//   return tab;
+// }
+
+// // link.addEventListener('click', async () => {
+// //     window.open("https://13xlabs.com");
+// // });
+
+// // linkgotoChatGPT.addEventListener('click', async () => {
+// //   window.open("https://chat.openai.com/");
+// // });
